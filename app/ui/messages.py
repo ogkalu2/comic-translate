6a95a2cb5d45f1f60762eea1e13343bf253aff06
ogkalu2 <115248977+ogkalu2@users.noreply.github.com>
2024-07-24 03:32:18 -0300

    Added Messages

---
+++ app/ui/messages.py
@@ -0,0 +1,75 @@
+from .dayu_widgets.message import MMessage
+from PySide6.QtCore import QCoreApplication
+
+class Messages:
+    @staticmethod
+    def show_api_key_translator_error(parent):
+        MMessage.error(
+            text=QCoreApplication.translate("Messages", "An API Key is required for the selected translator.\nGo to Settings > Credentials to set one"),
+            parent=parent,
+            duration=None,
+            closable=True
+        )
+
+    @staticmethod
+    def show_api_key_ocr_error(parent):
+        MMessage.error(
+            text=QCoreApplication.translate("Messages", "An API Key is required for the selected OCR.\nGo to Settings > Credentials to set one"),
+            parent=parent,
+            duration=None,
+            closable=True
+        )
+
+    @staticmethod
+    def show_api_key_ocr_gpt4v_error(parent):
+        MMessage.error(
+            text=QCoreApplication.translate("Messages", "Default OCR for the selected Source Language is GPT-4o\nwhich requires an API Key. Go to Settings > Credentials > GPT to set one"),
+            parent=parent,
+            duration=None,
+            closable=True
+        )
+
+    @staticmethod
+    def show_endpoint_url_error(parent):
+        MMessage.error(
+            text=QCoreApplication.translate("Messages", "An Endpoint URL is required for Microsoft OCR.\nGo to Settings > Credentials > Microsoft to set one"),
+            parent=parent,
+            duration=None,
+            closable=True
+        )
+
+    @staticmethod
+    def show_deepl_ch_error(parent):
+        MMessage.error(
+            text=QCoreApplication.translate("Messages", "DeepL does not translate to Traditional Chinese"),
+            parent=parent,
+            duration=None,
+            closable=True
+        )
+
+    @staticmethod
+    def show_googlet_ptbr_error(parent):
+        MMessage.error(
+            text=QCoreApplication.translate("Messages", "Google Translate does not support Brazillian Portuguese"),
+            parent=parent,
+            duration=None,
+            closable=True
+        )
+
+    @staticmethod
+    def show_translation_complete(parent):
+        MMessage.success(
+            text=QCoreApplication.translate("Messages", "Comic has been Translated!"),
+            parent=parent,
+            duration=None,
+            closable=True
+        )
+
+    @staticmethod
+    def select_font_error(parent):
+        MMessage.error(
+            text=QCoreApplication.translate("Messages", "No Font selected.\nGo to Settings > Text Rendering > Font to select or import one "),
+            parent=parent,
+            duration=None,
+            closable=True
+        )
