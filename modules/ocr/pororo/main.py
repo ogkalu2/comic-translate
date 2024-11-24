73ec70bc6f4d54b3a2d6a31027b113219f8986b2
ogkalu2 <115248977+ogkalu2@users.noreply.github.com>
2024-11-23 22:55:12 -0400

    remove matplotlib dependency

--- modules/ocr/pororo/main.py
+++ modules/ocr/pororo/main.py
@@ -1,7 +1,7 @@
 import cv2
 from .pororo import Pororo
 from .pororo.pororo import SUPPORTED_TASKS
-from .utils.image_util import plt_imshow, put_text
+#from .utils.image_util import plt_imshow, put_text
 import warnings
 
 warnings.filterwarnings('ignore')
@@ -43,43 +43,43 @@ class PororoOcr:
     def get_img_path(self):
         return self.img_path
 
-    def show_img(self):
-        plt_imshow(img=self.img_path)
+    # def show_img(self):
+    #     plt_imshow(img=self.img_path)
 
-    def show_img_with_ocr(self):
-        if isinstance(self.img_path, str):
-            img = cv2.imread(self.img_path)
-        else:
-            img = self.img_path
-        roi_img = img.copy()
+    # def show_img_with_ocr(self):
+    #     if isinstance(self.img_path, str):
+    #         img = cv2.imread(self.img_path)
+    #     else:
+    #         img = self.img_path
+    #     roi_img = img.copy()
 
-        for text_result in self.ocr_result['bounding_poly']:
-            text = text_result['description']
-            tlX = text_result['vertices'][0]['x']
-            tlY = text_result['vertices'][0]['y']
-            trX = text_result['vertices'][1]['x']
-            trY = text_result['vertices'][1]['y']
-            brX = text_result['vertices'][2]['x']
-            brY = text_result['vertices'][2]['y']
-            blX = text_result['vertices'][3]['x']
-            blY = text_result['vertices'][3]['y']
+    #     for text_result in self.ocr_result['bounding_poly']:
+    #         text = text_result['description']
+    #         tlX = text_result['vertices'][0]['x']
+    #         tlY = text_result['vertices'][0]['y']
+    #         trX = text_result['vertices'][1]['x']
+    #         trY = text_result['vertices'][1]['y']
+    #         brX = text_result['vertices'][2]['x']
+    #         brY = text_result['vertices'][2]['y']
+    #         blX = text_result['vertices'][3]['x']
+    #         blY = text_result['vertices'][3]['y']
 
-            pts = ((tlX, tlY), (trX, trY), (brX, brY), (blX, blY))
+    #         pts = ((tlX, tlY), (trX, trY), (brX, brY), (blX, blY))
 
-            topLeft = pts[0]
-            topRight = pts[1]
-            bottomRight = pts[2]
-            bottomLeft = pts[3]
+    #         topLeft = pts[0]
+    #         topRight = pts[1]
+    #         bottomRight = pts[2]
+    #         bottomLeft = pts[3]
 
-            cv2.line(roi_img, topLeft, topRight, (0, 255, 0), 2)
-            cv2.line(roi_img, topRight, bottomRight, (0, 255, 0), 2)
-            cv2.line(roi_img, bottomRight, bottomLeft, (0, 255, 0), 2)
-            cv2.line(roi_img, bottomLeft, topLeft, (0, 255, 0), 2)
-            roi_img = put_text(roi_img, text, topLeft[0], topLeft[1] - 20, font_size=15)
+    #         cv2.line(roi_img, topLeft, topRight, (0, 255, 0), 2)
+    #         cv2.line(roi_img, topRight, bottomRight, (0, 255, 0), 2)
+    #         cv2.line(roi_img, bottomRight, bottomLeft, (0, 255, 0), 2)
+    #         cv2.line(roi_img, bottomLeft, topLeft, (0, 255, 0), 2)
+    #         roi_img = put_text(roi_img, text, topLeft[0], topLeft[1] - 20, font_size=15)
 
-            # print(text)
+    #         # print(text)
 
-        plt_imshow(["Original", "ROI"], [img, roi_img], figsize=(16, 10))
+    #     plt_imshow(["Original", "ROI"], [img, roi_img], figsize=(16, 10))
 
 
 if __name__ == "__main__":
