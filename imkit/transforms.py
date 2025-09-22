ca7f5d62b07a3545ce5e81538ba00a9aeee9d66e
ogkalu2 <115248977+ogkalu2@users.noreply.github.com>
2025-09-22 14:46:03 -0300

    fix issue with connected components with stats that caused pororo ocr detects to miss some words

--- imkit/transforms.py
+++ imkit/transforms.py
@@ -338,7 +338,7 @@ def connected_components(image: np.ndarray, connectivity: int = 4) -> tuple:
     # mh.label returns the labeled image and the number of objects (excluding background)
     labeled, num_labels = mh.label(image > 0, Bc=Bc)
 
-    return num_labels, labeled
+    return num_labels+1, labeled
 
 
 def connected_components_with_stats(image: np.ndarray, connectivity: int = 4) -> tuple:
@@ -367,7 +367,7 @@ def connected_components_with_stats(image: np.ndarray, connectivity: int = 4) ->
         # Background only
         stats = np.array([[0, 0, image.shape[1], image.shape[0], image.size]], dtype=np.int32)
         centroids = np.array([[ (image.shape[1]-1)/2.0, (image.shape[0]-1)/2.0 ]], dtype=np.float64)
-        return 0, labeled, stats, centroids
+        return 1, labeled, stats, centroids
 
     # 3. Calculate statistics for all labels at once (including background label 0)
     # The output of these functions is an array where the index corresponds to the label.
@@ -408,8 +408,8 @@ def connected_components_with_stats(image: np.ndarray, connectivity: int = 4) ->
     height[sizes == 0] = 0
     
     stats = np.stack([xmin, ymin, width, height, sizes], axis=1).astype(np.int32)
-    
-    return num_labels, labeled, stats, centroids
+
+    return num_labels+1, labeled, stats, centroids
 
 
 def line(
