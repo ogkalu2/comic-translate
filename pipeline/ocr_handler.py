4f69bbcb94f3232fd5f5ec061d8cbeee64a371b1
ogkalu2 <115248977+ogkalu2@users.noreply.github.com>
2025-11-28 20:53:13 -0400

    Update ocr_handler.py

--- pipeline/ocr_handler.py
+++ pipeline/ocr_handler.py
@@ -20,7 +20,9 @@ class OCRHandler:
         if self.main_page.image_viewer.hasPhoto() and self.main_page.image_viewer.rectangles:
             image = self.main_page.image_viewer.get_image_array()
             ocr_model = self.main_page.settings_page.get_tool_selection('ocr')
-            device = resolve_device(self.main_page.settings_page.is_gpu_enabled())
+            device = resolve_device(
+                self.main_page.settings_page.is_gpu_enabled()
+            )
             cache_key = self.cache_manager._get_ocr_cache_key(image, source_lang, ocr_model, device)
             
             if single_block:
