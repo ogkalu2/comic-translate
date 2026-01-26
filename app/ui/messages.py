ca18c7e7919e75a773945eea8f1a6a37d064a898
ogkalu2 <115248977+ogkalu2@users.noreply.github.com>
2026-01-25 22:55:52 -0400

    Update messages.py

--- app/ui/messages.py
+++ app/ui/messages.py
@@ -35,7 +35,7 @@ class Messages:
         MMessage.error(
             text=QCoreApplication.translate(
                 "Messages",
-                "Please sign in or sign up via Settings > Account to continue. (Advanced users can set API keys in Settings > Advanced)."
+                "Please sign in or sign up via Settings > Account to continue."
             ),
             parent=parent,
             duration=None,
